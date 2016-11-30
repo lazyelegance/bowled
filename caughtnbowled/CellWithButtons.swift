b6275fb786baa9f3adaf249bdcbd94bdd8a38551
Ezra <ezrabathini@gmail.com>
2016-11-30 23:41:30 +1300

    new ui setup initial :sparkles:

---
+++ caughtnbowled/CellWithButtons.swift
@@ -0,0 +1,29 @@
+//
+//  CellWithButtons.swift
+//  Bowled
+//
+//  Created by Ezra Bathini on 30/11/16.
+//  Copyright © 2016 Ezra Bathini. All rights reserved.
+//
+
+import UIKit
+import Material
+
+
+class CellWithButtons: UITableViewCell {
+
+    @IBOutlet weak var btn1: FlatButton!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- caughtnbowled/Match.swift
+++ caughtnbowled/Match.swift
@@ -118,15 +118,23 @@ struct Match {
         
         
         
-//        if upcomingMatches.count > 0 {
-//            var i = 0
-//            for match in upcomingMatches {
-//                if match.hasRelDate && i < 2 && match.hometeamName != "To Be Decided" && match.awayteamName != "To Be Decided" {
-//                    topMatches.append(match)
-//                    i += 1
-//                }
-//            }
-//        }
+        if upcomingMatches.count > 0 {
+            var i = 0
+            for match in upcomingMatches {
+                if match.hasRelDate && i < 2 && match.hometeamName != "To Be Decided" && match.awayteamName != "To Be Decided" {
+                    topMatches.append(match)
+                    i += 1
+                }
+            }
+        }
+        
+        if topMatches.count > 0 {
+            let series = topMatches[0].seriesId
+            var dummy = Match(matchId: 0, seriesId: series, status: .dummy_series)
+            dummy.seriesName = topMatches[0].seriesName
+            topMatches.insert(dummy, at: 1)
+        }
+        
         
         
 
