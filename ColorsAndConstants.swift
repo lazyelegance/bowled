2b020d644a571f4ca3e732dedd85620d1e1daa91
Ezra <ezrabathini@gmail.com>
2017-01-08 10:27:17 +1300

    updated teams /types list

--- ColorsAndConstants.swift
+++ ColorsAndConstants.swift
@@ -12,6 +12,8 @@ import Foundation
 import UIKit
 import Material
 
+// MARK: - Constants
+
 let kMatchListHeaderHeight: CGFloat = 90
 var kTableHeaderHeight: CGFloat = 80
 var kHeaderSubviewHeight: CGFloat = 280
@@ -22,40 +24,15 @@ let keysDictionary = NSDictionary(contentsOfFile: Bundle.main.path(forResource: 
 let resoursesURLString = keysDictionary?.object(forKey: "resoursesURLString") as! String
 var resoursesURL = URL(string: resoursesURLString)
 
+// MARK: - Colors
 
-let peterrock = UIColor(red: 52.0/255.0, green: 152.0/255.0, blue: 219.0/255.0, alpha: 1.0)
-let peterrock_2 = UIColor(red: 41.0/255.0, green: 128.0/255.0, blue: 185.0/255.0, alpha: 1.0)
-
-let midnightblue = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0)
-let midnightblue_2 = UIColor(red: 52.0/255.0, green: 73.0/255.0, blue: 94.0/255.0, alpha: 1.0)
-
-let pomegranate = UIColor(red: 192.0/255.0, green: 57.0/255.0, blue: 43.0/255.0, alpha: 1.0)
-let pomegranate_2 = UIColor(red: 231.0/255.0, green: 76.0/255.0, blue: 60.0/255.0, alpha: 1.0)
-
-
-let belizehole = UIColor(red: 41.0/255.0, green: 128.0/255.0, blue: 185.0/255.0, alpha: 1.0)
-let nephritis = UIColor(red: 39.0/255.0, green: 174.0/255.0, blue: 96.0/255.0, alpha: 1.0)
-
-let greensea = UIColor(red: 22.0/255.0, green: 160.0/255.0, blue: 133.0/255.0, alpha: 1.0)
-//rgba(22, 160, 133,1.0)
-let silver = UIColor(red: 189.0/255.0, green: 195.0/255.0, blue: 199.0/255.0, alpha: 1.0)
-
-var clearcolor = UIColor.clear //
-var navBarColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1)
-
-
-
-let whitecolor = UIColor.white
-
-
+let clearcolor = Color.clear
+let whitecolor = Color.white
 let mainColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0) //Color.indigo.base
 let secondaryColor = UIColor(red: 52.0/255.0, green: 73.0/255.0, blue: 94.0/255.0, alpha: 1.0)//rgba(52, 73, 94,1.0)// Color.darkGray
 let txtColor = Color.white
 
-
-
-let favoriteTeamsList = ["INDIA", "AUSTRALIA", "Sri Lanka"]
-
+// MARK: - Swift Loader
 
 var swiftLoaderConfig : SwiftLoader.Config = SwiftLoader.Config()
 
@@ -83,19 +60,6 @@ extension UserDefaults {
 }
 
 extension UIColor {
-    
-    func secondaryColor() -> UIColor {
-        switch self {
-        case peterrock:
-            return peterrock_2
-        case midnightblue:
-            return midnightblue_2
-        case pomegranate:
-            return pomegranate_2
-        default:
-            return self
-        }
-    }
     
     public convenience init?(hexString: String) {
         let r, g, b, a: CGFloat
@@ -233,98 +197,47 @@ public enum MatchStatus {
     
 }
 
-public enum AppSetting {
-    case showOnlyInternational
-    case favoriteTeamSelect
-    case showOnlyFavorite
-    
-    func to_String() -> String {
-        switch self {
-        case .showOnlyInternational:
-            return "Show International Matches Only"
-        case .favoriteTeamSelect:
-            return "Select Favorite Team"
-        case .showOnlyFavorite:
-            return "Show Only Selected"
-        }
-    }
-    
-    func defaultsKeyString() -> String {
-        switch self {
-        case .showOnlyInternational:
-            return "InternationalMatchesOnly"
-        case .favoriteTeamSelect:
-            return "FavoriteTeam"
-        case .showOnlyFavorite:
-            return "ShowFavoriteOnly"
-        }
-    }
-    
-    func has_switch() -> Bool {
-        switch self {
-        case .showOnlyInternational:
-            return true
-        case .favoriteTeamSelect:
-            return false
-        case .showOnlyFavorite:
-            return true
-        }
-    }
-    
-    func selectionList() -> [String] {
-        switch self {
-        case .favoriteTeamSelect:
-            return favoriteTeamsList
-        default:
-            return [""]
-        }
-    }
-    
-}
 
 
-public enum SideMenuItem {
-    case allMatches
-    case liveMatches
-    case upcoming
-    case calender
-    case completed
-    
-    func to_String() -> String {
-        switch self {
-        case .allMatches:
-            return "All Matches"
-        case .liveMatches:
-            return "Live Matches"
-        case .upcoming:
-            return "Upcoming Matches"
-        case .completed:
-            return "Completed Matches"
-        case .calender:
-            return "Calendar"
-        }
-    }
-    
-    
-    
-}
 
-public enum SideMenuFilterItem {
-    case teams
-    case series
-    
-    
-    func to_String() -> String {
-        switch self {
-        case .teams:
-            return "TEAMS"
-        case .series:
-            return "SERIES"
-            
-        }
-    }
-    
-}
+var teams = [Team(name: "ADELAIDE STRIKERS", shortName: "STR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Adelaide-Strikers.ashx", teamColor: "#00b0de", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "BRISBANE HEAT", shortName: "HEA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Brisbane-Heat.ashx", teamColor: "#00b3bc", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "HOBART HURRICANES", shortName: "HUR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Hobart-Hurricanes.ashx", teamColor: "#773cb0", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "MELBOURNE RENEGADES", shortName: "REN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Renegades.ashx", teamColor: "#df0048", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "MELBOURNE STARS", shortName: "STA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Stars.ashx", teamColor: "#339e00", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "PERTH SCORCHERS", shortName: "SCO", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Perth-Scorchers.ashx", teamColor: "#f58400", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "SYDNEY SIXERS", shortName: "SIX", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Sixers.ashx", teamColor: "#dd1b9d", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "SYDNEY THUNDER", shortName: "THU", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Thunder.ashx", teamColor: "#9fc000", isInternational: false, isWomensTeam :false, teamType: "BBL"),
+             Team(name: "ADELAIDE STRIKERS WOMEN", shortName: "STR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Adelaide-Strikers.ashx", teamColor: "#00b0de", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "BRISBANE HEAT WOMEN", shortName: "HEA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Brisbane-Heat.ashx", teamColor: "#00b3bc", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "HOBART HURRICANES WOMEN", shortName: "HUR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Hobart-Hurricanes.ashx", teamColor: "#773cb0", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "MELBOURNE RENEGADES WOMEN", shortName: "REN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Renegades.ashx", teamColor: "#df0048", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "MELBOURNE STARS WOMEN", shortName: "STA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Stars.ashx", teamColor: "#339e00", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "PERTH SCORCHERS WOMEN", shortName: "SCO", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Perth-Scorchers.ashx", teamColor: "#f58400", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "SYDNEY SIXERS WOMEN", shortName: "SIX", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Sixers.ashx", teamColor: "#dd1b9d", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "SYDNEY THUNDER WOMEN", shortName: "THU", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Thunder.ashx", teamColor: "#9fc000", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
+             Team(name: "AUSTRALIA", shortName: "AUS", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Australia.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "BANGLADESH", shortName: "BAN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Bangladesh.ashx", teamColor: "#00794f", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "ENGLAND", shortName: "ENG", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/England.ashx", teamColor: "#dc210a", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "INDIA", shortName: "IND", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/India.ashx", teamColor: "#0099cc", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "IRELAND", shortName: "IRE", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Ireland.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "NEW ZEALAND", shortName: "NZ", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/New-Zealand.ashx", teamColor: "#363636", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "PAKISTAN", shortName: "PAK", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Pakistan.ashx", teamColor: "#084118", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "SOUTH AFRICA", shortName: "SA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/South-Africa.ashx", teamColor: "#007a4d", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "SRI LANKA", shortName: "SL", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Sri-Lanka.ashx", teamColor: "#403fa8", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "WEST INDIES", shortName: "WI", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/West-Indies.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "ZIMBABWE", shortName: "ZIM", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Zimbabwe.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
+             Team(name: "AUSTRALIA WOMEN", shortName: "AUS", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Australia.ashx", teamColor: "#c8a000", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "BANGLADESH WOMEN", shortName: "BAN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Bangladesh.ashx", teamColor: "#00794f", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "ENGLAND WOMEN", shortName: "ENG", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/England.ashx", teamColor: "#dc210a", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "INDIA WOMEN", shortName: "IND", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/India.ashx", teamColor: "#0099cc", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "IRELAND WOMEN", shortName: "IRE", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Ireland.ashx", teamColor: "#dc210a", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "NEW ZEALAND WOMEN", shortName: "NZ", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/New-Zealand.ashx", teamColor: "#363636", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "PAKISTAN WOMEN", shortName: "PAK", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Pakistan.ashx", teamColor: "#084118", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "SOUTH AFRICA WOMEN", shortName: "SA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/South-Africa.ashx", teamColor: "#007a4d", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "SRI LANKA WOMEN", shortName: "SL", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Sri-Lanka.ashx", teamColor: "#403fa8", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "WEST INDIES WOMEN", shortName: "WI", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/West-Indies.ashx", teamColor: "#00794f", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
+             Team(name: "ZIMBABWE WOMEN", shortName: "ZIM", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Zimbabwe.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN")]
 
 
 
--- caughtnbowled/SideMenuController.swift
+++ caughtnbowled/SideMenuController.swift
@@ -34,62 +34,17 @@ class SideMenuController: UIViewController, UITableViewDelegate, UITableViewData
     
     var isFullmenu = true // as opposed to false when selecting favorite team
     
-    //temp
-    
-    
-   
-    var teams = [Team(name: "ADELAIDE STRIKERS", shortName: "STR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Adelaide-Strikers.ashx", teamColor: "#00b0de", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "BRISBANE HEAT", shortName: "HEA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Brisbane-Heat.ashx", teamColor: "#00b3bc", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "HOBART HURRICANES", shortName: "HUR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Hobart-Hurricanes.ashx", teamColor: "#773cb0", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "MELBOURNE RENEGADES", shortName: "REN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Renegades.ashx", teamColor: "#df0048", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "MELBOURNE STARS", shortName: "STA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Stars.ashx", teamColor: "#339e00", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "PERTH SCORCHERS", shortName: "SCO", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Perth-Scorchers.ashx", teamColor: "#f58400", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "SYDNEY SIXERS", shortName: "SIX", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Sixers.ashx", teamColor: "#dd1b9d", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "SYDNEY THUNDER", shortName: "THU", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Thunder.ashx", teamColor: "#9fc000", isInternational: false, isWomensTeam :false, teamType: "BBL"),
-    Team(name: "ADELAIDE STRIKERS WOMEN", shortName: "STR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Adelaide-Strikers.ashx", teamColor: "#00b0de", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "BRISBANE HEAT WOMEN", shortName: "HEA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Brisbane-Heat.ashx", teamColor: "#00b3bc", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "HOBART HURRICANES WOMEN", shortName: "HUR", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Hobart-Hurricanes.ashx", teamColor: "#773cb0", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "MELBOURNE RENEGADES WOMEN", shortName: "REN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Renegades.ashx", teamColor: "#df0048", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "MELBOURNE STARS WOMEN", shortName: "STA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Melbourne-Stars.ashx", teamColor: "#339e00", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "PERTH SCORCHERS WOMEN", shortName: "SCO", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Perth-Scorchers.ashx", teamColor: "#f58400", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "SYDNEY SIXERS WOMEN", shortName: "SIX", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Sixers.ashx", teamColor: "#dd1b9d", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "SYDNEY THUNDER WOMEN", shortName: "THU", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/BBL/Sydney-Thunder.ashx", teamColor: "#9fc000", isInternational: false, isWomensTeam :true, teamType: "WBBL"),
-    Team(name: "AUSTRALIA", shortName: "AUS", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Australia.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "BANGLADESH", shortName: "BAN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Bangladesh.ashx", teamColor: "#00794f", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "ENGLAND", shortName: "ENG", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/England.ashx", teamColor: "#dc210a", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "INDIA", shortName: "IND", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/India.ashx", teamColor: "#0099cc", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "IRELAND", shortName: "IRE", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Ireland.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "NEW ZEALAND", shortName: "NZ", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/New-Zealand.ashx", teamColor: "#363636", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "PAKISTAN", shortName: "PAK", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Pakistan.ashx", teamColor: "#084118", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "SOUTH AFRICA", shortName: "SA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/South-Africa.ashx", teamColor: "#007a4d", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "SRI LANKA", shortName: "SL", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Sri-Lanka.ashx", teamColor: "#403fa8", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "WEST INDIES", shortName: "WI", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/West-Indies.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "ZIMBABWE", shortName: "ZIM", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Zimbabwe.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :false, teamType: "INTERNATIONAL"),
-    Team(name: "AUSTRALIA WOMEN", shortName: "AUS", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Australia.ashx", teamColor: "#c8a000", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "BANGLADESH WOMEN", shortName: "BAN", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Bangladesh.ashx", teamColor: "#00794f", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "ENGLAND WOMEN", shortName: "ENG", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/England.ashx", teamColor: "#dc210a", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "INDIA WOMEN", shortName: "IND", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/India.ashx", teamColor: "#0099cc", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "IRELAND WOMEN", shortName: "IRE", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Ireland.ashx", teamColor: "#dc210a", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "NEW ZEALAND WOMEN", shortName: "NZ", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/New-Zealand.ashx", teamColor: "#363636", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "PAKISTAN WOMEN", shortName: "PAK", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Pakistan.ashx", teamColor: "#084118", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "SOUTH AFRICA WOMEN", shortName: "SA", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/South-Africa.ashx", teamColor: "#007a4d", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "SRI LANKA WOMEN", shortName: "SL", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Sri-Lanka.ashx", teamColor: "#403fa8", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "WEST INDIES WOMEN", shortName: "WI", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/West-Indies.ashx", teamColor: "#00794f", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN"),
-    Team(name: "ZIMBABWE WOMEN", shortName: "ZIM", logoString: "http://www.cricket.com.au/-/media/Logos/Teams/International/Zimbabwe.ashx", teamColor: "#00503c", isInternational: true, isWomensTeam :true, teamType: "INTERNATIONAL WOMEN")]
-    
-    
-    var teamNames = [String]()
+    var teamTypes = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = mainColor
         
-        teamNames = Array(Set(teams.map { $0.teamType }.flatMap { $0 })).sorted(by: { (a, b) -> Bool in
+        teamTypes = Array(Set(teams.map { $0.teamType }.flatMap { $0 })).sorted(by: { (a, b) -> Bool in
             return (a == "INTERNATIONAL")
         })
-        // Do any additional setup after loading the view.
         
-        let menuTitles = isFullmenu ? ["TEAMS", "SERIES'", "MATCH TYPES", "SETTINGS"] : teamNames //Array(Set(teams.map { $0.teamType }.flatMap { $0 }))
+        let menuTitles = isFullmenu ? ["TEAMS", "SERIES'", "MATCH TYPES", "SETTINGS"] : teamTypes //Array(Set(teams.map { $0.teamType }.flatMap { $0 }))
         mainMenu = HMSegmentedControl(sectionTitles: menuTitles)
         
         mainMenu.addTarget(self, action: #selector(SideMenuController.mainMenuChangedValue(_:)), for: UIControlEvents.valueChanged)
@@ -146,11 +101,16 @@ class SideMenuController: UIViewController, UITableViewDelegate, UITableViewData
     func prepareTableViewData() {
         
         
-        teamsList = Array(Set([matches.map{ $0.hometeamName }, matches.map{ $0.awayteamName }].flatMap { $0 }))
+        //teamsList = Array(Set([matches.map{ $0.hometeamName }, matches.map{ $0.awayteamName }].flatMap { $0 }))
+        // fixed teams
+        for type in teamTypes {
+            teamsList = [teamsList, teams.filter { $0.teamType == type }.map { $0.name }].flatMap { $0 }
+        }
         
         seriesList = Array(Set(matches.map{ $0.seriesName }))
         
-        matchTypeList = Array(Set(matches.map{ $0.cmsMatchType }))
+        matchTypeList = [ "Test", "One-Day International", "T20 International", "BBL","WBBL","First-Class"] //Array(Set(matches.map{ $0.cmsMatchType }))
+        
         
         tableView.reloadData()
     }
@@ -195,7 +155,7 @@ class SideMenuController: UIViewController, UITableViewDelegate, UITableViewData
                 return 0
             }
         } else {
-            return teams.filter({ $0.teamType == teamNames[mainMenu.selectedSegmentIndex]}).count
+            return teams.filter({ $0.teamType == teamTypes[mainMenu.selectedSegmentIndex]}).count
         }
     }
     
@@ -216,7 +176,7 @@ class SideMenuController: UIViewController, UITableViewDelegate, UITableViewData
                 break
             }
         } else {
-            menuCell.menuItem = teams.filter({ $0.teamType == teamNames[mainMenu.selectedSegmentIndex]})[indexPath.row].name
+            menuCell.menuItem = teams.filter({ $0.teamType == teamTypes[mainMenu.selectedSegmentIndex]})[indexPath.row].name
         }
         
         return menuCell
@@ -237,9 +197,7 @@ class SideMenuController: UIViewController, UITableViewDelegate, UITableViewData
                 break
             }
         } else {
-            
-            
-            defaults?.set(teams.filter({ $0.teamType == teamNames[mainMenu.selectedSegmentIndex]})[indexPath.row].name.uppercased(), forKey: "favoriteTeamName")
+            defaults?.set(teams.filter({ $0.teamType == teamTypes[mainMenu.selectedSegmentIndex]})[indexPath.row].name.uppercased(), forKey: "favoriteTeamName")
             self.navigationController?.popViewController(animated: true)
         }
         
